1f1f6f23b4139ec8df69006cd055a2cb437d0b03
vasuy <vasuyaragorla7@gmail.com>
2025-04-20 16:01:20 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -6,17 +6,17 @@ check_root
 echo "please enter DB password:"
 read -s mysql_root_password
 
-dnf install mysql-server -y &>>$logfile
-validate $? "installing mysql server"
+dnf install mysql-serrrdver -y &>>$logfile
+#validate $? "installing mysql server"
 
 systemctl enable mysqld &>>$logfile
-validate $? "eabling mysql server"
+#validate $? "eabling mysql server"
 
 systemctl start mysqld &>>$logfile
-validate $? "starting mysql server"
+#validate $? "starting mysql server"
 
 mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$logfile
-validate $? "root password setting up"
+#validate $? "root password setting up"
 
 #below code will be useful for idempotent nature
 #mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$logfile
