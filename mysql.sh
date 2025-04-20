e48684650387ad6b6541889f09e70bed278cacf7
vasuy <vasuyaragorla7@gmail.com>
2025-04-20 16:09:29 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -6,7 +6,7 @@ check_root
 echo "please enter DB password:"
 read -s mysql_root_password
 
-dnf install mysql-serrrdver -y &>>$logfile
+dnf install mysql-server -y &>>$logfile
 #validate $? "installing mysql server"
 
 systemctl enable mysqld &>>$logfile
