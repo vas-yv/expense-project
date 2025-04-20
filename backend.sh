520f1846d0f8e9a6c3e06ecc7cc5f601b0a1dee1
vasuy <vasuyaragorla7@gmail.com>
2025-04-20 16:18:53 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -1,61 +1,62 @@
 #!/bin/bash
 
 source ./comman.sh
+
 check_root
 
 echo "please enter DB password:"
 read -s mysql_root_password
 
 dnf module disable nodejs -y &>>$logfile
-validate $? "disable existing nodejs"
+#validate $? "disable existing nodejs"
 
 dnf module enable nodejs:20 -y &>>$logfile
-validate $? "enabling nodejs 20"
+#validate $? "enabling nodejs 20"
 
 dnf install nodejs -y &>>$logfile
-validate $? "installing nodejs"
+#validate $? "installing nodejs"
 
 id expense &>>$logfile
 if [ $? -ne 0 ]
 then 
     useradd expense &>>$logfile
-    validate $? "creating expense user"
+    #validate $? "creating expense user"
 else
      echo -e "user already created..$Y skipping $N"
 fi         
 
 mkdir -p /app 
-validate $? "creating app dir"
+#validate $? "creating app dir"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$logfile
-validate $? "downloading backend code"
+#validate $? "downloading backend code"
 
 cd /app
 rm -rf /app/*
 unzip /tmp/backend.zip &>>$logfile
-validate $? "unzip backend code"
+#validate $? "unzip backend code"
 
 npm install &>>$logfile
-validate $? "installing dependencies"
+#validate $? "installing dependencies"
 
 cp /home/ec2-user/expense-project/backend.service /etc/systemd/system/backend.service
 
 systemctl daemon-reload &>>$logfile
-validate $? "daemon reload"
+#validate $? "daemon reload"
 
 systemctl start backend &>>$logfile
-validate $? "starting backend"
+#validate $? "starting backend"
 
 
 systemctl enable backend &>>$logfile
-validate $? "enabling backend"
+#validate $? "enabling backend"
 
 
 dnf install mysql -y &>>$logfile
-validate $? "installing mysql"
+#validate $? "installing mysql"
 
-mysql -h 172.31.82.88 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$logfile
-validate $? "configuring"
+mysql -h 172.31.20.212 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$logfile
+#validate $? "configuring"
 
 systemctl restart backend &>>$logfile
-validate $? "restarting backend"
+#validate $? "restarting backend"
